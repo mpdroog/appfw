a27c413f65678966341d5671b64fabfe2c4a8efb
MP Droog <mpdroog@icloud.com>
2022-07-09 12:35:32 +0200

    Bugfix. Default not used without "

--- config/config.go
+++ config/config.go
@@ -4,7 +4,7 @@ type Config struct {
 	/* Path/to/state */
 	State string `default:"./state.tsv"`
 	/* Amount of entries to keep track of */
-	StateSize int `default:1024`
+	StateSize int `default:"1024"`
 
 	/** Host:port addr */
 	Listen string `default:"127.0.0.1:1337"`
