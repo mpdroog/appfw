36fa105013184348481787aa7d9cf945259d297f
MP Droog <mpdroog@icloud.com>
2022-07-12 16:49:02 +0200

    Util. Add version-var to build-script

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ set -e
 set -x
 set -u
 
-env GOOS=linux GOARCH=amd64 go build
+env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -installsuffix cgo -ldflags "-extldflags \"-static\" -X main.version=$(git describe --always --long --dirty --all)-$(date +%Y-%m-%d-%H:%M)" -o appfw
 echo -n $'\003' | dd bs=1 count=1 seek=7 conv=notrunc of=./afd
